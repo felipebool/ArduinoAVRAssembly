b4dc3a86bb32a1c07d2a16fb2529d5790f8b9e3f
felipe <bolzin@gmail.com>
2016-05-28 09:51:34 -0300

    atmega328p definitions

---
+++ tools/328pdef.s
@@ -0,0 +1,76 @@
+;
+; Port registers -----------------------
+
+; PortB [8..13]
+.equ portb, 0x05
+.equ ddrb,  0x04
+.equ pinb,  0x03
+
+; PortC [a0..a5]
+.equ portc, 0x08
+.equ ddrc,  0x07
+.equ pinc,  0x06
+
+; PortD [0..7]
+.equ portd, 0x0b
+.equ ddrd,  0x0a
+.equ pind,  0x09
+; --------------------------------------
+
+
+; Timers -------------------------------
+
+; Timer0
+;.equ tcnt0,
+;.equ ocr0a,
+;.equ ocr0b,
+
+; Timer1
+;.equ tcnt1,
+;.equ ocr1a,
+;.equ ocr1b,
+
+; Timer2
+;.equ tcnt2,
+;.equ ocr2a,
+;.equ ocr2b,
+; --------------------------------------
+
+
+
+; General Purpose Register -------------
+
+.equ r0,  0x00
+.equ r1,  0x01
+.equ r2,  0x02
+.equ r3,  0x03
+.equ r4,  0x04
+.equ r5,  0x05
+.equ r6,  0x06
+.equ r7,  0x07
+.equ r8,  0x08
+.equ r9,  0x09
+.equ r10, 0x0a
+.equ r11, 0x0b
+.equ r12, 0x0c
+.equ r13, 0x0d
+.equ r14, 0x0e
+.equ r15, 0x0f
+.equ r16, 0x10
+.equ r17, 0x11
+.equ r18, 0x12
+.equ r19, 0x13
+.equ r20, 0x14
+.equ r21, 0x15
+.equ r22, 0x16
+.equ r23, 0x17
+.equ r24, 0x18
+.equ r25, 0x19
+.equ r26, 0x1a
+.equ r27, 0x1b
+.equ r28, 0x1c
+.equ r29, 0x1d
+.equ r30, 0x1e
+.equ r31, 0x1f
+; --------------------------------------
+
